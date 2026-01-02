64ddd6df9af24c51b6d79714f71c64c32d6a0294
shunpcho <kshuchi0203@gmail.com>
2026-01-03 05:05:49 +0900

    refactor: img_size

--- src/vit_unet/models/build_model.py
+++ src/vit_unet/models/build_model.py
@@ -4,14 +4,16 @@ from vit_unet.config.model_config import VitunetConfig
 from vit_unet.models.model import ViTUNet
 
 
-def get_vit_unet(model_string: Literal["lite", "base", "large"], verbose: bool = False) -> ViTUNet:
+def get_vit_unet(
+    model_string: Literal["lite", "base", "large"], verbose: bool = False, im_size: int | None = None
+) -> ViTUNet:
     if model_string == "lite":
         config = VitunetConfig(
             depth=2,
             depth_te=1,
             size_bottleneck=2,
             preprocessing="conv",
-            im_size=224,
+            im_size=im_size if im_size is not None else 256,
             patch_size=16,
             num_channels=3,
             hidden_dim=64,
@@ -29,7 +31,7 @@ def get_vit_unet(model_string: Literal["lite", "base", "large"], verbose: bool =
             depth_te=2,
             size_bottleneck=2,
             preprocessing="conv",
-            im_size=256,
+            im_size=im_size if im_size is not None else 256,
             patch_size=32,
             num_channels=3,
             hidden_dim=128,
@@ -47,7 +49,7 @@ def get_vit_unet(model_string: Literal["lite", "base", "large"], verbose: bool =
             depth_te=4,
             size_bottleneck=4,
             preprocessing="conv",
-            im_size=256,
+            im_size=im_size if im_size is not None else 256,
             patch_size=32,
             num_channels=3,
             hidden_dim=256,
