43e57268e6f4a319583fa55666f1446a789bee7f
shunpcho <kshuchi0203@gmail.com>
2026-04-09 23:47:19 +0900

    tests: add model test code

--- tests/test_model.py
+++ tests/test_model.py
@@ -34,4 +34,22 @@ def test_vit_unet_model():
 
     # Test Encoder Block
     # Level 1
-    x = model._encoder_patches(embedded_patches)
+    print("Testing Encoder Blocks")
+    x_patch, encoder_skip = model._encode_patches(embedded_patches)
+    print(f"Encoder output x_patch shape: {x_patch.shape}")
+    for i, skip in enumerate(encoder_skip):
+        print(f"Skip connection {i + 1} shape: {skip.shape}")
+
+    # Test Bottleneck
+    print("Testing Bottleneck")
+    x_bottleneck = model._process_bottleneck(x_patch)
+    print(f"Bottleneck output x_bottleneck shape: {x_bottleneck.shape}")
+
+    # Test Decoder Block
+    print("Testing Decoder Blocks")
+    x_decoded = model._decode_patches(x_bottleneck, encoder_skip)
+    print(f"Decoder output x_decoded shape: {x_decoded.shape}")
+
+
+if __name__ == "__main__":
+    test_vit_unet_model()
